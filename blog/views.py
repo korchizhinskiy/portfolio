646d4d851c8863b6bc82dd73625bee5f2bfb7d5c
korchizhinskiy <korchizhinskiy@gmail.com>
2022-08-20 20:13:42 +0300

    Return render, send context with publication objects

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from .models import BlogNew
+from blog.services import get_all_from_database
 
-from django.http import HttpResponse
 
-
-def index(requests):
-    return HttpResponse("Главная страница")
+def index(request):
+    news = get_all_from_database(BlogNew)
+    return render(request, template_name='blog/index.html', context={"news": news})
