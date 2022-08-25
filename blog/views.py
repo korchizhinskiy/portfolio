5df38df69bd4f0001ad9d3ac70b6dd8084901822
korchizhinskiy <korchizhinskiy@gmail.com>
2022-08-25 10:51:08 +0300

    Change functions to class view

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import BlogNew
 from blog.services import get_all_from_database
 
 
-def index(request):
-    news = get_all_from_database(BlogNew)
-    return render(request, template_name='blog/index.html', context={"news": news})
+class BlogPage(ListView):
+    """Blog Page View with all news."""
+    model = BlogNew
+    template_name: str = 'blog/index.html'
+    context_object_name = 'blog'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Блог'
+        return context
+
+    def get_queryset(self):
+        """Return filtered query set."""
+        return BlogNew.objects.filter(is_published=True)
