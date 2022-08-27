3865f40d08b44cddfcf6045119c4e9496feea509
korchizhinskiy <korchizhinskiy@gmail.com>
2022-08-27 11:20:26 +0300

    Change register to decoration, add fields to models

--- blog/admin.py
+++ blog/admin.py
@@ -3,10 +3,20 @@ from django.contrib import admin
 from .models import BlogNew, NewCategory
 
 
-class BlogNewAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'reate_date', 'update_date', 'is_published')
+@admin.register(BlogNew)
+class PublicationAdmin(admin.ModelAdmin):
+    """Class of publication new in admin site."""
+    list_display = ('id', 'title', 'category', 'create_date', 'upgrade_date', 'is_published')
     list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    search_help_text = "Начните вводить заголовок/содержание."
 
-admin.site.register(BlogNew)
-admin.site.register(NewCategory)
+
+@admin.register(NewCategory)
+class CategoryAdmin(admin.ModelAdmin):
+    """Class of new category in admin site."""
+    list_display = ('pk', 'category_name')
+    list_display_links = ('pk', 'category_name')
+    search_fields = ('category_name',)
+    search_help_text = "Начните вводить название категории."
 
