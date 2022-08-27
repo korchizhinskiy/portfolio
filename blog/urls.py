ef1d9fb1d18be7d40210648392f095f288f67179
korchizhinskiy <korchizhinskiy@gmail.com>
2022-08-27 21:07:36 +0300

    Add path to onew of news and news of categories

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import BlogPage, ViewNew
+from .views import BlogPage, BlogPageOneOfCategory, ViewNew
 
 urlpatterns = [
         path('', BlogPage.as_view(), name="home"),
-        path('blog_new/<int:pk>', ViewNew.as_view(), name="blog_new")
-        ]
+        path('blog_new/<int:pk>', ViewNew.as_view(), name="blog_new"),
+        path('category/<int:pk>', BlogPageOneOfCategory.as_view(), name="category"),
+]
 
