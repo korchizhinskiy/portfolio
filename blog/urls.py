870a39e9a631815eb0724e6bb5c7c9b230e873a0
korchizhinskiy <korchizhinskiy@gmail.com>
2022-08-27 11:24:09 +0300

    Add path to one of the news

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BlogPage
+from .views import BlogPage, ViewNew
 
 urlpatterns = [
-        path('', BlogPage.as_view(), name="home")
+        path('', BlogPage.as_view(), name="home"),
+        path('blog_new/<int:pk>', ViewNew.as_view(), name="blog_new")
         ]
 
