a867d6b43d951a5d1a46056879ab9a238b7960cc
korchizhinskiy <korchizhinskiy@gmail.com>
2022-08-28 18:15:04 +0300

    Add home url

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import BlogPage, BlogPageOneOfCategory, ViewNew
+from .views import BlogPage, BlogPageOneOfCategory, home_page, ViewNew
 
 urlpatterns = [
-        path('', BlogPage.as_view(), name="blog"),
+        path('', home_page, name="home"),
+        path('blog', BlogPage.as_view(), name="blog"),
         path('blog_new/<int:pk>', ViewNew.as_view(), name="blog_new"),
         path('category/<int:pk>', BlogPageOneOfCategory.as_view(), name="category"),
 ]
