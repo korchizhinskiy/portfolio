70e1783e27a72a43e1c679812d8d35cf1c2ad87b
korchizhinskiy <korchizhinskiy@gmail.com>
2022-08-28 09:10:50 +0300

    Change home path to blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import BlogPage, BlogPageOneOfCategory, ViewNew
 
 urlpatterns = [
-        path('', BlogPage.as_view(), name="home"),
+        path('', BlogPage.as_view(), name="blog"),
         path('blog_new/<int:pk>', ViewNew.as_view(), name="blog_new"),
         path('category/<int:pk>', BlogPageOneOfCategory.as_view(), name="category"),
 ]
